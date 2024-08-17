3ad26b94117717ddc8e5ed03e2852128d625ac2e
K0ndrich <123kondrich@gmail.com>
2024-08-17 17:26:48 +0300

    edited

--- project/orders/admin.py
+++ project/orders/admin.py
@@ -1,4 +1,9 @@
 # Файл для управление панелью администратора и регистрации моделей(таблиц баз данных)
+
+# django
 from django.contrib import admin
 
+# my_project
+from orders.models import SalesOrder
 
+admin.site.register(SalesOrder)
